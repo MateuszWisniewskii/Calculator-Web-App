b1b94f2de27cb97a45e7429299b224629043930b
MateuszWisniewskii <mdw180703@gmail.com>
2025-01-19 19:44:16 +0100

    .

---
+++ CalculatorWeb/src/main/java/pl/polsl/calculator/model/Calculation.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.polsl.calculator.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author mdw18
+ */
+@Entity
+public class Calculation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+        private double operand1;
+    private double operand2;
+    private String operator;
+    private double result;
+
+    public Calculation() {}
+
+    public Calculation(double operand1, double operand2, String operator, double result) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operator = operator;
+        this.result = result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(double operand1) {
+        this.operand1 = operand1;
+    }
+
+    public double getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(double operand2) {
+        this.operand2 = operand2;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Calculation)) {
+            return false;
+        }
+        Calculation other = (Calculation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pl.polsl.calculator.model.Calculation[ id=" + id + " ]";
+    }
+    
+}
